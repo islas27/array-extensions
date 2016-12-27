48b177b8598c7b95297d165a799ffd6909f66e87
Jonathan Islas <islas.27@gmail.com>
2016-12-27 13:23:14 -0700

    Changed a variable name to be less confusing

--- aee.js
+++ aee.js
@@ -85,7 +85,7 @@ Array.prototype.count = function (spec) {
 
 Array.prototype.sum = function (spec) {
   const len = this.length
-  let sumSpec = (typeof spec === 'function') ? spec : spec => spec
+  let sumSpec = (typeof spec === 'function') ? spec : value => value
   let result = (!isNaN(sumSpec(this[0], 0)) || this[0] === undefined) ? 0 : ''
   for (let i = 0; i < len; i += 1) {
     result += sumSpec(this[i], i)
